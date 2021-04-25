7ef11707aa0c22303374eb19fa35b4ac87604455
Tobias Heitor Goettert <tobias.goettert@compasso.com.br>
2021-04-25 17:25:17 -0300

    Import de fonts, adição de icones do expo, ajuste na interface da pagina inicial e utilização do AppLoading do expo.

--- src/pages/Welcome.tsx
+++ src/pages/Welcome.tsx
@@ -1,52 +1,79 @@
 import React from 'react';
-import { SafeAreaView, Text, Image, TouchableOpacity, StyleSheet } from 'react-native';
+import {
+    SafeAreaView,
+    Text,
+    Image,
+    TouchableOpacity,
+    StyleSheet,
+    Dimensions,
+    View
+} from 'react-native';
+import { Feather } from '@expo/vector-icons'
 
 import wateringImg from '../assets/watering.png'
 import colors from '../styles/colors';
+import fonts from '../styles/fonts';
 
 export function Welcome() {
     return (
         <SafeAreaView style={styles.container}>
-            <Text style={styles.title}>
-                Gerencie {'\n'}
-                suas plantas {'\n'}
-                de forma fácil
-            </Text>
-
-            <Image source={wateringImg} style={styles.image} />
-
-            <Text style={styles.subTitle}>
-                Não esqueça mais de regar suas plantas.
-                Nós cuidamos de lembrar você sempre que precisar.
-            </Text>
-
-            <TouchableOpacity style={styles.buttons}>
-                <Text style={styles.buttonText}>
-                    >
+            <View style={styles.wrapper}>
+                <Text style={styles.title}>
+                    Gerencie {'\n'}
+                    suas plantas de {'\n'}
+                    forma fácil
                 </Text>
-            </TouchableOpacity>
+
+                <Image
+                    source={wateringImg}
+                    style={styles.image}
+                    resizeMode="contain"
+                />
+
+                <Text style={styles.subTitle}>
+                    Não esqueça mais de regar suas plantas.
+                    Nós cuidamos de lembrar você sempre que precisar.
+                </Text>
+
+                <TouchableOpacity style={styles.buttons} >
+                    <Feather
+                        name="chevron-right"
+                        style={styles.buttonIcon}
+                    />
+                </TouchableOpacity>
+            </View>
         </SafeAreaView>
     )
 }
 
 const styles = StyleSheet.create({
     container: {
+        flex: 1
+    },
+    wrapper: {
         flex: 1,
         alignItems: 'center',
-        justifyContent: 'space-between'
+        justifyContent: 'space-around',
+        paddingHorizontal: 20
     },
     title: {
-        fontSize: 32,
+        fontSize: 28,
         fontWeight: 'bold',
         textAlign: 'center',
         color: colors.heading,
-        marginTop: 38
+        marginTop: 38,
+        fontFamily: fonts.heading,
+        lineHeight: 34
     },
     subTitle: {
         textAlign: 'center',
         fontSize: 18,
         paddingHorizontal: 20,
-        color: colors.heading
+        color: colors.heading,
+        fontFamily: fonts.text
+    },
+    image: {
+        height: Dimensions.get('window').width * 0.7,
     },
     buttons: {
         backgroundColor: colors.green,
@@ -57,12 +84,8 @@ const styles = StyleSheet.create({
         height: 56,
         width: 56
     },
-    image: {
-        width: 292,
-        height: 284
-    },
-    buttonText: {
+    buttonIcon: {
         color: colors.white,
-        fontSize: 24
+        fontSize: 32
     }
 });
